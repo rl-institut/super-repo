08740b0d1029649afc345ece0fe259e9d599da1b
jh-RLI <jonas.huber@rl-institut.de>
2023-08-16 18:18:07 +0200

    add common calculator functionality that can be tested #21, also include reuse identifier

---
+++ super_repo/example_calculator.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+""" Example implementation of test for the calculater functionality to demonstrate TDD. 
+
+
+SPDX-FileCopyrightText: 2023 Jonas Huber <@jh-RLI>
+SPDX-FileCopyrightText: © Reiner Lemoine Institut
+
+SPDX-License-Identifier: MIT
+"""
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
