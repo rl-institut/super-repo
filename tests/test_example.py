b56fc5931119c0d37d58b1d85f27f46f73fb8950
jh-RLI <jonas.huber@rl-institut.de>
2023-08-16 18:34:16 +0200

    format document #21

--- tests/test_example.py
+++ tests/test_example.py
@@ -11,17 +11,21 @@ SPDX-License-Identifier: MIT
 
 from super_repo.example_calculator import add, subtract, multiply, divide
 
+
 def test_addition():
     result = add(3, 4)
     assert result == 7
+
 
 def test_subtraction():
     result = subtract(10, 5)
     assert result == 5
 
+
 def test_multiplication():
     result = multiply(2, 6)
     assert result == 12
+
 
 def test_division():
     result = divide(15, 3)
