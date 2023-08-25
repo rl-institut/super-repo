3edd47fc4fe1b3b201844f1b69fa0d25246d2d92
Ludee <Ludwig.Huelk@rl-institut.de>
2023-08-25 12:17:59 +0200

    Add copyright notice #33

---
+++ super_repo/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Example.
+
+Examples for the calculater functionality to demonstrate TDD.
+
+SPDX-FileCopyrightText: Jonas Huber © Reiner Lemoine Institut <https://github.com/jh-RLI>
+SPDX-FileCopyrightText: Ludwig Hülk © Reiner Lemoine Institut <https://github.com/Ludee>
+SPDX-License-Identifier: MIT
+"""
