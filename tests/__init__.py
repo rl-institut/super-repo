7d2c027f1e9f0169fed70165580fb514a4cbce19
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2024-11-27 20:24:21 +0100

    add init test #61

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Example.
+
+
+SPDX-FileCopyrightText: Jonas Huber <@jh-RLI> © Reiner Lemoine Institut
+SPDX-FileCopyrightText: Ludwig Hülk <@Ludee> © Reiner Lemoine Institut
+SPDX-License-Identifier: MIT
+"""
